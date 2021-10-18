3d3581127ea9bec91d113d76dee80e578dfca85c
awiggin1995 <92637084+awiggin1995@users.noreply.github.com>
2021-10-18 18:26:19 +0200

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/awiggin1995/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('awiggin1995')
+            node / gitConfigEmail('awiggin1995@gmail.com')
         }
     }
     triggers {
